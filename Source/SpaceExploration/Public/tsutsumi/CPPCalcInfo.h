a60fe544372aca988a9135d980010a86dfd31408
mikenchi <mikenti337564@gmail.com>
2024-06-09 18:18:59 +0900

    [add]

---
+++ Source/SpaceExploration/Public/tsutsumi/CPPCalcInfo.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CPPCalcInfo.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FStatus {
+
+	GENERATED_BODY()
+
+	float HP;
+	float AttackPower;
+	float DefencePower;
+	float Speed;
+};
---
+++ Source/SpaceExploration/Public/tsutsumi/Element.h
@@ -0,0 +1,15 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Element.generated.h"
+
+
+UENUM(BlueprintType) 
+enum class EElement
+{
+	fire,
+	water,
+	wind
+};
