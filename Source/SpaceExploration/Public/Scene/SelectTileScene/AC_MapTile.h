bdefdb740b2ccbd002098d567e80661da0ef6c7b
ItoTatsuhiro <ito.3229.wo@gmail.com>
2024-06-23 16:11:24 +0900

    マスベースの作成、マス種類のEnumClass作成、スターターコンテンツ追加

--- Source/SpaceExploration/Public/Scene/SelectTileScene/AC_MapTile.h
+++ Source/SpaceExploration/Public/Scene/SelectTileScene/AC_MapTile.h
@@ -9,6 +9,7 @@
 
 #include "AC_MapTile.generated.h"
 
+//********************************************************************
 // マップ上のマスのベースクラス
 // 抽象クラス、それぞれのマスのクラスに継承して用いる
 UCLASS( Abstract )
@@ -27,13 +28,23 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	// マスで実行するイベントの関数
-	// 継承先でこの関数をオーバーライドして処理を作成する
-	virtual void TileEvent() PURE_VIRTUAL(AAC_MapTileBase::TileEvent, );
-
 
 	// マスの種類の変数
+	UPROPERTY(EditAnywhere)
 	E_TILE_TYPE tileType_ = E_TILE_TYPE::NONE;
+
+
+	//------------------------------------------------------------------
+	// メッシュ関連
+
+	// SceneComponent
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<USceneComponent> defaultSceneRoot_;
+
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UStaticMeshComponent> staticMesh_;
+
 
 
 public:	
@@ -42,4 +53,14 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+
+	// マスで実行するイベントの関数
+	// 継承先でこの関数をオーバーライドして処理を作成する
+	virtual void TileEvent() PURE_VIRTUAL(AAC_MapTileBase::TileEvent, );
+
+	// スタティックメッシュをセットする関数
+	// 引数：fileName...スタティックメッシュの保存先のパス
+	// 
+	// 使用例）SetStaticMesh(TEXT("/Game/StarterContent/Shapes/Shape_Sphere"));
+	void SetStaticMesh(const TCHAR* fileName);
 };
