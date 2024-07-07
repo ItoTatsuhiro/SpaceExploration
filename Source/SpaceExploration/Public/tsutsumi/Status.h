fb99f9fd005d0ebf54d6228859f0c2056bc7aad2
mikenchi <mikenti337564@gmail.com>
2024-07-07 12:43:57 +0900

    [add]ステータスクラスの改良と惑星をランダムに決めるコードを完成させました

---
+++ Source/SpaceExploration/Public/tsutsumi/Status.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Status.generated.h"
+
+USTRUCT(BlueprintType)
+struct FStatus {
+
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float HP;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float MaxHp;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float AttackPower;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float DefencePower;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float Speed;
+
+    UPROPERTY(EditAnywhere)
+    FString PlayerName;
+
+
+    
+
+   
+    
+
+
+
+    // オーバーロードされた代入演算子
+    FStatus& operator=(const FStatus& status) {
+        if (this != &status) {
+            HP = status.HP;
+            MaxHp = status.MaxHp;
+            AttackPower = status.AttackPower;
+            DefencePower = status.DefencePower;
+            Speed = status.Speed;
+        }
+        return *this;
+    }
+};
+
+// グローバルなFStatus変数
+FStatus PlayerStatus;
+FStatus Copy = PlayerStatus;
+
