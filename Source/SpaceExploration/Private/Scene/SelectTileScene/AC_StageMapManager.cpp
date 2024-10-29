0b9504d3abac6bb7d86b4fe802fc5447a14f0ab8
ItoTatsuhiro <ito.3229.wo@gmail.com>
2024-10-29 16:14:00 +0900

    ・マス選択中の処理を一連の流れとして処理できるように作成 ・MapTileBaseのTickの不具合を修正

--- Source/SpaceExploration/Private/Scene/SelectTileScene/AC_StageMapManager.cpp
+++ Source/SpaceExploration/Private/Scene/SelectTileScene/AC_StageMapManager.cpp
@@ -7,6 +7,7 @@
 #include "../../../Public/Scene/SelectTileScene/AC_MapTileItem.h"
 #include "../../../Public/Scene/SelectTileScene/AC_MapTile_Battle.h"
 #include "../../../Public/Character/PlayerCharacter.h"
+#include "PlayerController/SelectPlanetPlayerController.h"
 
 #include <Kismet/GameplayStatics.h>
 
@@ -34,7 +35,6 @@ AAC_StageMapManager::AAC_StageMapManager()
     tileObjectComponent_ = CreateDefaultSubobject<UChildActorComponent>(TEXT("tileObjectComponent"));
     tileObjectComponent_->SetupAttachment(RootComponent);
     
-    
 }
 
 
@@ -50,9 +50,9 @@ void AAC_StageMapManager::BeginPlay()
 
     UE_LOG(LogTemp, Log, TEXT("BeginPlay"));
 
-    PlayerCharacter_ = Cast<APlayerCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+    playerCharacter_ = Cast<APlayerCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
 
-    if (!PlayerCharacter_) {
+    if (!playerCharacter_) {
         UKismetSystemLibrary::PrintString(this, "PlayerCharacter_ is nullptr", true, true, FColor::Red, 3.f);
         UE_LOG(LogClass, Error, TEXT("PlayerCharacter_ is nullptr"));
         return;
@@ -65,6 +65,10 @@ void AAC_StageMapManager::BeginPlay()
     // デリゲート初期化
     createTileDel_ = FSequenceDelegate::CreateUObject(this, &AAC_StageMapManager::SeqCreateTile);
     selectTileDel_ = FSequenceDelegate::CreateUObject(this, &AAC_StageMapManager::SeqSelectTile);
+    playerMoveBeginDel_ = FSequenceDelegate::CreateUObject(this, &AAC_StageMapManager::SeqPlayerMoveBegin);
+    playerMoveIdleDel_ = FSequenceDelegate::CreateUObject(this, &AAC_StageMapManager::SeqPlayerMoveIdle);
+    executeTileEventDel_ = FSequenceDelegate::CreateUObject(this, &AAC_StageMapManager::SeqExecuteTileEvent);
+
 
     // 最初に実行する関数を指定
     sequenceManager_ = NewObject<USequenceManager>();
@@ -75,6 +79,10 @@ void AAC_StageMapManager::BeginPlay()
     }
 
     sequenceManager_->ChangeSequence(createTileDel_);
+
+
+    // 自身をplayerControllerにセット
+    Cast<ASelectPlanetPlayerController>(UGameplayStatics::GetPlayerController(this, 0))->SetStageMapManager(this);
 
 }
 
@@ -91,6 +99,9 @@ void AAC_StageMapManager::Tick(float DeltaTime)
         // シーケンスの更新
         sequenceManager_->updateSequence(DeltaTime);
 
+        // 入力の状態を確認
+        CheckClickInput();
+
     }
     else {
 
@@ -98,6 +109,48 @@ void AAC_StageMapManager::Tick(float DeltaTime)
     }
 
 }
+
+
+
+//------------------------------------------------------------------------------------
+// 入力を確認する用の関数
+
+
+
+
+
+// クリック入力を外部から伝えるための関数
+// playerControllerでクリックされたときの処理としてバインドする用
+void AAC_StageMapManager::OnClickInput() {
+
+    // 入力が既にある場合は処理しない
+    if (isClickInput_) { return; }
+
+    // 入力があった状態にする
+    isClickInput_ = true;
+
+}
+
+
+// クリック入力を確認する関数
+// クリック入力を必要とする処理と、update関数で毎回isClickInputを確認する
+bool AAC_StageMapManager::CheckClickInput() {
+
+    // 入力の状態が入っていないときは処理しない
+    if (!isClickInput_) { return false; }
+
+    else {
+
+        // 入力の状態をリセットする
+        isClickInput_ = false;
+
+        // trueを返す
+        return true;
+    }
+
+}
+
+
 
 
 // -----------------------------------------------------------------------------------------------------
@@ -156,13 +209,14 @@ void AAC_StageMapManager::SeqSelectTile(const float delta_time) {
     // カーソルと重なっているオブジェクトを取得
     AActor* hoveredObj = PerformRaycast();
 
-    if ( hoveredObj != nullptr && hoveredObj->IsA(AAC_MapTileBase::StaticClass())) {
+    // 重なっているオブジェクトがマスのクラスを継承している場合
+    if ( hoveredObj && hoveredObj->IsA(AAC_MapTileBase::StaticClass())) {
 
         // 重なっているオブジェクトをMapTileBaseのクラスにキャスト
-        AActor* hoveredTile = Cast<AAC_MapTileBase>(hoveredObj);
+        AAC_MapTileBase* hoveredTile = Cast<AAC_MapTileBase>(hoveredObj);
 
         // 現在変数として置いてあるものと比較
-        if (hoveredTile_ != hoveredTile) {
+        if (hoveredTile_ != hoveredTile && hoveredTile) {
             
             // 置き換え
             hoveredTile_ = hoveredTile;
@@ -185,10 +239,130 @@ void AAC_StageMapManager::SeqSelectTile(const float delta_time) {
 
     }
 
+    // クリックされていない状態の時は処理しない
+    if (!CheckClickInput()) { return; }
+    // 重なっているマスが存在しない場合は処理しない
+    if (!hoveredTile_) { return; }
+
+    // ログを表示
+    UE_LOG(LogTemp, Log, TEXT("クリック"));
+
+    // 重なっているマスを選択中のマスとして登録
+    selectTile_ = hoveredTile_;
+
+    // 実行するシーケンスを切り替え
+    // 切り替え先：移動開始シーケンス
+    sequenceManager_->ChangeSequence(playerMoveBeginDel_);
+
+}
+
+
+
+
+// プレイヤー移動シーケンス
+void AAC_StageMapManager::SeqPlayerMoveBegin(const float delta_time) {
+
+
+    // プレイヤーの移動を行う
+    // プレイヤーから、クリックした際の処理を取ってくる
+
+
+    // マスが選択されていないときは処理しない
+    if (!selectTile_) {
+
+        // ログを表示
+        UE_LOG(LogTemp, Log, TEXT("マスが選択されていません"));
+
+        return;
+    }
+
+
+
+
+    // プレイヤーが待機中でないときは処理しない
+    if (playerCharacter_->GetCharaterActState() != ECharacterActState::Idle) {
+        return;
+    }
+
+
+    // 移動先の座標を計算
+    FVector movePos = selectTile_->GetActorLocation() + playerCharacter_->GetActorRotation().RotateVector(moveTargetOffset_);
+
+    // 移動開始の指示
+    playerCharacter_->BeginMoveTargetLocation( movePos );
+
+
+
+    // 実行するシーケンスを切り替え
+    // 切り替え先：移動終了待機シーケンス
+    sequenceManager_->ChangeSequence(playerMoveIdleDel_);
+
+
+}
+
+
+
+
+
+// プレイヤー移動待機シーケンス
+void AAC_StageMapManager::SeqPlayerMoveIdle(const float delta_time) {
+
+
+    // 移動が終了した待機状態でないときは処理しない
+    if (playerCharacter_->GetCharaterActState() != ECharacterActState::Idle) {
+        return;
+    }
+
+
+    // 移動終了後
+
+    // 実行するシーケンスを切り替え
+    // 切り替え先：マスのイベント実行シーケンス
+    sequenceManager_->ChangeSequence(executeTileEventDel_);
+
+}
+
+
+
+
+// マスのイベント実行シーケンス
+void AAC_StageMapManager::SeqExecuteTileEvent(const float delta_time) {
+
+
+    // マスのイベントを実行する処理（仮）
+
+
+    // マスが選択されていないときは処理しない
+    if (!selectTile_) { 
+
+        // ログを表示
+        UE_LOG(LogTemp, Log, TEXT("マスが選択されていません"));
+
+        return;
+    }
+
+
+    // **********************************
+    // イベント実行処理
+    // **********************************
+
+    // マスのイベントを実行
+    selectTile_->TileEvent();
+
+
+
+
+    // ※※※ イベント実行後 ※※※
+
+    // 実行するシーケンスを切り替え
+    // 切り替え先：マス選択シーケンス
+    sequenceManager_->ChangeSequence(selectTileDel_);
 
 
 
 }
+
+
 
 
 
@@ -268,7 +442,6 @@ void AAC_StageMapManager::CreateTileObjArray(TArray<int> createTileNumArray)
 
                 break;
             }
-
 
         }
 
